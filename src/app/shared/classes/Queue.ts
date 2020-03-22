a24c2197a38d9d6b25b9384ac1d45d2064a8caf9
mayank <mayank8005@gmail.com>
2020-03-22 06:30:09 -0700

    using array.length instead of maintaining our own variable

--- src/app/shared/classes/Queue.ts
+++ src/app/shared/classes/Queue.ts
@@ -2,22 +2,18 @@ export class Queue {
 
     // storage structure for queue
     private store: any[] = [];
-    // store no of item present in queue
-    private itemCount = 0;
 
     public enqueue(item: any): void {
-        this.itemCount += 1;
         this.store.push(item);
     }
 
     public dequeue(): any {
-        this.itemCount -= 1;
         return this.store.length ? this.store.shift() : null;
     }
 
     // return total number of items in queue
     public getItemCount(): number {
-        return this.itemCount;
+        return this.store.length;
     }
 
 }
